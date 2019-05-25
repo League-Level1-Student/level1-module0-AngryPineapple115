bf7a5ffed14d81f7af954ff6cc9adc54d90d2e69
league <league@wts9.attlocal.net>
2019-05-24 18:26:41 -0700

    Methods #2 completed, Methods #3 in-progress

---
+++ src/MethodsThree.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class MethodsThree {
+	public static void main(String[] args) {
+
+		// 1.)
+		
+		eat(food);
+
+		getDinnerTime(3.5);
+
+		getFavoriteFood(Bob);
+
+		chomp();
+
+		isTasty();
+		
+		// 2.)
+		
+		
+
+	}
+}
--- src/MethodsTwo.java
+++ src/MethodsTwo.java
@@ -4,18 +4,43 @@ public class MethodsTwo {
 
 	public static void main(String[] args) {
 
-		
-		
-		
-		
-	public void findThePresent() {
-			
-			
-			
-			
-			
+		findThePresent();
+		doPigsEat();
+		getMyLuckyNumber();
+		howManyEggs();
+
+	}
+
+	static void findThePresent() {
+		String present = JOptionPane.showInputDialog(null,
+				"What present do you want for Christmas?\n\nThis is from a computer and definitely not from a human.");
+
+		System.out.println("Your friend wants " + present + " for christmas.");
+	}
+
+	static void doPigsEat() {
+		String edible = JOptionPane.showInputDialog("Enter a food that you think are edible to pigs.");
+		if (edible.equalsIgnoreCase("roots")) {
+			System.out.println("TRUE");
+		} else {
+			System.out.println("FALSE");
 		}
-		
-		
+	}
+
+	static void getMyLuckyNumber() {
+		String lucky = JOptionPane.showInputDialog("What is your lucky number?");
+		System.out.println("The lucky number is " + lucky + ".");
+	}
+
+	static void howManyEggs() {
+		String mom = JOptionPane.showInputDialog("How many eggs does Mom need?");
+		String dad = JOptionPane.showInputDialog("How many eggs does dad need?");
+		String baby = JOptionPane.showInputDialog("How many eggs does the baby need?");
+		int momint = Integer.parseInt(mom);
+		int dadint = Integer.parseInt(dad);
+		int babyint = Integer.parseInt(baby);
+		int totaleggs = momint + dadint + babyint;
+		JOptionPane.showMessageDialog(null, "The whole family needs " + totaleggs + ".");
+		System.out.println("The whole family needs " + totaleggs + ".");
 	}
 }
