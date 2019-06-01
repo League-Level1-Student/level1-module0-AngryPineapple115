3c9f3ca02893419ebbdcb82d14579f42378a4bf4
league <league@wts9.attlocal.net>
2019-05-31 18:28:23 -0700

    Methods Three completed

--- src/MethodsThree.java
+++ src/MethodsThree.java
@@ -1,10 +1,11 @@
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class MethodsThree {
 	public static void main(String[] args) {
 
 		// 1.)
-		
+
 		eat(food);
 
 		getDinnerTime(3.5);
@@ -14,10 +15,43 @@ public class MethodsThree {
 		chomp();
 
 		isTasty();
-		
+
 		// 2.)
-		
-		
+
+		JFrame window = getWindow();
+
+		int slices = getNumberOfPizzaSlicesLeft();
+
+		String faveVeggie = getFavoriteVegetable("Mom");
+
+		peel("potato");
+
+		boolean healthyOrNot = isHealthy("avocado");
+
+		simmer("soup");
 
 	}
+
+	static JFrame getWindow() {
+		return null;
+	}
+
+	static int getNumberOfPizzaSlicesLeft() {
+		return 0;
+	}
+
+	static String getFavoriteVegetable(String string) {
+		return null;
+	}
+
+	static void peel(String string) {
+	}
+
+	static boolean isHealthy(String string) {
+		return false;
+	}
+
+	static void simmer(String string) {
+	}
+
 }
